92f707f88d6a52d5ed3c014c56c47ca346ddb969
redex2 <redex2@redex2.dev>
2023-05-09 10:06:38 +0200

    add get.php

---
+++ get.php
@@ -0,0 +1,30 @@
+<?php
+function error()
+{
+    header("HTTP/1.0 404 Not Found");
+    require("error.html");
+    exit(0);
+}
+
+if (
+    isset($_SERVER['HTTP_REFERER']) &&
+    isset($_SERVER['REQUEST_METHOD']) &&
+    isset($_SERVER['REQUEST_URI']) &&
+    $_SERVER['HTTP_REFERER'] === "https://sudoku.redex2.dev/" &&
+    $_SERVER['REQUEST_METHOD'] === "GET" &&
+    $_SERVER['REQUEST_URI'] === "/get.php"
+) {
+
+    try {
+        $db = new SQLite3(".db/sudoku.db", SQLITE3_OPEN_READONLY);
+        $out = $db->query("SELECT puzzle FROM sudoku ORDER BY RANDOM() LIMIT 1;");
+        $puzzle = $out->fetchArray();
+        if ($puzzle != false)
+            echo $puzzle[0];
+    } catch (Exception) {
+        error();
+    }
+} else {
+    error();
+}
+?>
